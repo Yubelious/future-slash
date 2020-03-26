13af62c092d73d5e110e9d55d6bf250bfce9999b
Ejeffers1239 <superejeffers@gmail.com>
2020-03-26 15:55:43 -0400

    Update The Selection.js
    Update for use as a userscript

--- The Selection.js
+++ The Selection.js
@@ -1,7 +1,24 @@
-langclass = "en";
-$( document ).ready(function() {
-	console.log("ready");
-	$('#lang-select').change(function(){
-		document.getElementById("flags").className = $("#lang-select").val();
-	});
-});
+// ==UserScript==
+// @name         The Selection.js
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Switches the flags in the language selection dropdown
+// @author       Ejeffers1239, Sock
+// @match        https://duelingnexus.com/*
+// @grant        none
+// @require      https://code.jquery.com/jquery-3.4.1.min.js
+// @updateURL    https://raw.githubusercontent.com/Yubelious/future-slash/master/The Selection.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var langclass = "en";
+    $( document ).ready(function() {
+        console.log("ready");
+        $('#lang-select').change(function(){
+            document.getElementById("flags").className = $("#lang-select").val();
+        });
+    });
+
+})();
