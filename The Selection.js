7d90b1f022a717d8421ede6ebaab218ebaa62767
Lanth Magnes <Yubelious@gmail.com>
2020-03-27 00:07:09 -0400

    Update The Selection.js

--- The Selection.js
+++ The Selection.js
@@ -1,24 +1,33 @@
-// ==UserScript==
-// @name         The Selection.js
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  Switches the flags in the language selection dropdown
-// @author       Ejeffers1239, Sock
-// @match        https://duelingnexus.com/*
-// @grant        none
-// @require      https://code.jquery.com/jquery-3.4.1.min.js
-// @updateURL    https://raw.githubusercontent.com/Yubelious/future-slash/master/The Selection.js
-// ==/UserScript==
 
-(function() {
-    'use strict';
+const langSelect = $("#lang-select");
+$(document).ready(function() {
+	console.log("ready");
+  let focused = false;
+  let updateArrow = function() {
+	  console.log("update");
+    $("#flags").toggleClass("active", focused);
+	document.getElementById("arrow").innerHTML = focused ? "⮙" : "⮛";
+  };
+  langSelect.click(function(ev) {
+    if (ev.target !== langSelect[0]) return;
+    focused = !focused;
+	console.log("select");
+    updateArrow();
+  });
 
-    var langclass = "en";
-    $( document ).ready(function() {
-        console.log("ready");
-        $('#lang-select').change(function(){
-            document.getElementById("flags").className = $("#lang-select").val();
-        });
-    });
-
-})();
+  let onDeselect = function() {
+	  console.log("deselect");
+    if (!focused) return;
+    focused = false;
+    updateArrow();
+  };
+  langSelect.focusout(function() {
+    onDeselect();
+  });
+  langSelect.change(function() {
+    onDeselect();
+  });
+  langSelect.find("option").click(function() {
+    onDeselect();
+  })
+});
