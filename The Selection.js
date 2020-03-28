1d2af436ce6e54515b00a0e8c24fd6747d557769
Ejeffers1239 <superejeffers@gmail.com>
2020-03-27 23:16:57 -0400

    Update The Selection.js
    Removed Link Arrow.js from The Selection.js

--- The Selection.js
+++ The Selection.js
@@ -5,35 +5,3 @@
             document.getElementById("flags").className = $("#lang-select").val();
         });
     });
-const langSelect = $("#lang-select");
-$(document).ready(function() {
-	console.log("ready");
-  let focused = false;
-  let updateArrow = function() {
-	  console.log("update");
-    $("#flags").toggleClass("active", focused);
-	document.getElementById("arrow").innerHTML = focused ? "⮙" : "⮛";
-  };
-  langSelect.click(function(ev) {
-    if (ev.target !== langSelect[0]) return;
-    focused = !focused;
-	console.log("select");
-    updateArrow();
-  });
-
-  let onDeselect = function() {
-	  console.log("deselect");
-    if (!focused) return;
-    focused = false;
-    updateArrow();
-  };
-  langSelect.focusout(function() {
-    onDeselect();
-  });
-  langSelect.change(function() {
-    onDeselect();
-  });
-  langSelect.find("option").click(function() {
-    onDeselect();
-  })
-});
